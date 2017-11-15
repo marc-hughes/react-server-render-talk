e52561a739b49a2ac8232256d751bf6c3334686b
Hughes, Marc <mhughes@rosettastone.com>
2017-11-14 19:38:23 -0500

    redux example

---
+++ reduxstate/src/index.js
@@ -0,0 +1,86 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {connect, Provider} from 'react-redux';
+import { createStore } from '../../../../../Library/Caches/typescript/2.6/node_modules/redux';
+
+
+// ----------------------------- Redux stuff
+let initial =  {
+  firstName: '',
+  lastName: '',
+  books: [],
+  loading: true
+};
+
+const reducer = (state=initial, action=null) => {
+  switch(action && action.type) {
+    case 'BOOKS_LOADED':
+      return {
+        ...state,
+        ...action.payload,
+        loading: ! state.firstName
+      }
+    case 'NAME_LOADED':
+      return {
+        ...state,
+        ...action.payload,
+        loading: ! state.books
+      }
+    default:
+      return state;
+  }
+}
+
+const actions = {
+  nameLoaded: (firstName, lastName) => ({type:'NAME_LOADED', payload:{firstName, lastName}}),
+  booksLoaded: (books) => ({type:'BOOKS_LOADED', payload:{books}})
+}
+
+
+// ----------------------------- React stuff
+const Book = (props) => <li>{props.name}</li>
+
+const BookList = (props) => (
+  <ul>
+    {props.books.map((book,idx) => <Book key={idx} name={book} />)}
+  </ul>
+);
+
+const App = (props) => props.loading ? <div>Loading...</div> :
+            <div>
+              Hello {props.first_name} {props.last_name} <br/>
+              <BookList books={props.books} />
+            </div>;
+
+
+
+// -------------------------------- Wire it together
+
+const AppContainer = connect((state) => state, actions)(App);
+
+const store = createStore(reducer, window.__INITIAL_STATE_)
+
+
+// --------------------------------- And render it out.
+
+
+ReactDOM.render(
+  <Provider store={store}>
+    <AppContainer />
+  </Provider>, document.getElementById('root'));
+
+
+// -------------------------------- Hit our API and load up some data
+
+const loadData = (store) => {
+  const url = 'https://runkit.io/marc-hughes/sample-api/branches/master';
+  fetch(url)
+    .then(response => response.json())
+    .then(result => {
+      store.dispatch(actions.booksLoaded(result.favorte_books));
+      store.dispatch(actions.nameLoaded(result.first_name, result.last_name));
+      console.log(store.getState())
+    })
+}
+
+loadData(store);
